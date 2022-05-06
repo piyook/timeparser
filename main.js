89c10400789b7d32404942cfc2b16f5dc7d7f40c
piyook <piyook@piyook.com>
2022-05-06 14:37:58 +0100

    Correct minor error in console test line 13 - test string didnt match console.log string

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ console.log("now()+10d+12h: ", parse("now()+10d+12h"));
 console.log("\n---------------------------------\n")
 
 //test complex compound input string
-console.log("now()+1y+3d-3h@m: ", parse("now()+1y+3d-3h+24m-3s@m"))
+console.log("now()+1y+3d-3h+24m-3s@m: ", parse("now()+1y+3d-3h+24m-3s@m"))
 console.log("now()+1d+3d: ", parse("now()+1d+3d"));
 console.log("now()+1d+3d-2d: ", parse("now()+1d+3d-2d"));
 console.log("now()@y: ", parse("now()@y"));
